ffbabb83f5dd3d301a4bc9aa768510669a78d71b
VaishnaviGabda <vaishnavigabda99@gmail.com>
2022-01-19 20:13:18 +0530

    checkout page ready

--- src/App.js
+++ src/App.js
@@ -5,13 +5,52 @@ import Header from './Header';
 import Home from './Home';
 import Login from './Login'
 import Checkout from './Checkout';
-import { BrowserRouter as Router , Switch ,Route} 
+import Payment  from './Payment';
+import { BrowserRouter as Router ,Switch ,Route, BrowserRouter} 
 from "react-router-dom"
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js";
 
+
+const promise = loadStripe("pk_test_51KJIolSB2rJifC6BxtIJMesPlSMgf9iBJE8QanvyjoOetb6VUvSZ7FtFAMc9wuajXzr4zBaRwXN2n3QTqRmFMu7o007oaizGTQ")
 
 function App() {
+
+  const [{} , dispatch] = useStateValue();
+
+  useEffect(()=>{
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('USER IS',authUser);
+
+      if (authUser){
+        //logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+
+      }else {
+        //logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+
+      }
+
+
+    })
+
+
+  },[])
+
   return (
     // BEM
+    <BrowserRouter>
     <Router>
     <div className="app">
       
@@ -20,9 +59,16 @@ function App() {
          <Login />
        </Route>
       <Route path="/checkout">
+        <Route path='/welcome' element={<Home/>} />
          <Header/>
         <Checkout/>
       </Route >
+      <Route path="/payment">
+          <Header/>
+          <Elements stripe={promise}>
+          <Payment/>
+          </Elements>
+        </Route>
      <Route path="/">
         <Header/>
         <Home/>
@@ -33,6 +79,7 @@ function App() {
 
     </div>
     </Router>
+    </BrowserRouter>
   );
 }
 export default App;
